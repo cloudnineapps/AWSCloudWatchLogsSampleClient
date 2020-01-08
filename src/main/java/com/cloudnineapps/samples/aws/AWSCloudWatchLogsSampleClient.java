6e2e91a0cf07d846c7f65137ca20b20da4447c29
Nitin Patil <nitin@cloudnineapps.com>
2020-01-08 18:46:13 -0500

    Formatting changes.

--- src/main/java/com/cloudnineapps/samples/aws/AWSCloudWatchLogsSampleClient.java
+++ src/main/java/com/cloudnineapps/samples/aws/AWSCloudWatchLogsSampleClient.java
@@ -1,5 +1,14 @@
 package com.cloudnineapps.samples.aws;
 
+/**
+ * Copyright © 2020 Cloud Nine Apps, LLC.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -78,6 +87,7 @@ public class AWSCloudWatchLogsSampleClient {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Launching the application...");
 		openCloudWatchLog();
+		// Sample log statements
 		log("Starting the app...");
 		log("Another message");
 		System.out.println("Execution completed.");
